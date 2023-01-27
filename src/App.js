d85ebe98f1a8d8d7ebfd853ab613a7c352e3ba71
Al Noor Istiak Mahmud <istiakmahmud1010@gmail.com>
2023-01-28 00:58:38 +0600

    change the palate

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div className='bg-indigo-500'>
-      <h2 className='text-green-500 text-center text-lg'>hellow text </h2>
+    <div>
+      hellow app
     </div>
   );
 };
